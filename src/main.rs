65fdd7090d52ef9e9b6c99a2005a3fdaa8358d67
wyatt <waustin47@outlook.com>
2024-02-04 20:30:29 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,186 @@
-use std::{fmt::Debug, ops::{AddAssign, Mul, Range}};
+use std::{fmt::Debug, ops::{AddAssign, Mul, Range}, time::Instant};
 
 use rustfft::{num_complex::{Complex, Complex32, Complex64, ComplexFloat}, num_traits::{float::FloatCore, zero, Float, FromPrimitive, Signed, Zero}, FftPlannerAvx};
 
 
 fn main() {
-    println!("Hello, world!");
-                
-    //let x = vec![Complex32::new(0.9597,0.),Complex32::new(0.3404,0.),Complex32::new(0.5853,0.),Complex32::new(0.2238,0.)];
 
-    let x:Vec<Complex32> = (1..21).into_iter().map(|x| Complex32::new(x as f32,0.)).collect();
+    let n = 200;
+    let x:Vec<Complex32> = (1..(n+1)).into_iter().map(|x| Complex32::new(x as f32,0.)).collect();
+    let wavelet = Wavelet::db2();
 
-    let lo_d = [-0.129409522550921,0.224143868041857,0.836516303737469,0.482962913144690];
-    let hi_d = [-0.482962913144690,0.836516303737469,-0.224143868041857,-0.129409522550921];
-    let lo_r = [0.482962913144690,0.836516303737469,0.224143868041857,-0.129409522550921];
-    let hi_r = [-0.129409522550921,-0.224143868041857,0.836516303737469,-0.482962913144690];
+    let mut wavedec = WaveDecPlanner::new(x.len(),3,wavelet);
+    let mut waverec = WaveRecPlanner::new(&wavedec);
+
+    let now = Instant::now();
+
+    let decomp = wavedec.process(&x);
+    let rec = waverec.process(&decomp);
+
+    let dur = now.elapsed();
+
+    println!("{:#?}",rec);
+    println!("elapsed: {} ms", dur.as_millis());
+}
+
+#[derive(Clone)]
+pub struct Wavelet<T> {
+    lo_d:Vec<T>,
+    hi_d:Vec<T>,
+    lo_r:Vec<T>,
+    hi_r:Vec<T>,
+}
+ impl<T> Wavelet<T>
+ where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static
+ {
+    pub fn db2() -> Self {
+        Self {
+            
+            lo_d: vec![
+                T::from_f64(-0.1294095225509214464043594716713414527475833892822265625).unwrap(),
+                T::from_f64(0.2241438680418573470287668669698177836835384368896484375).unwrap(),
+                T::from_f64(0.836516303737468991386094785411842167377471923828125).unwrap(),
+                T::from_f64(0.482962913144690253464119678028509952127933502197265625).unwrap()
+            ],
+            hi_d: vec![
+                T::from_f64(-0.482962913144690253464119678028509952127933502197265625).unwrap(),
+                T::from_f64(0.836516303737468991386094785411842167377471923828125).unwrap(),
+                T::from_f64(-0.2241438680418573470287668669698177836835384368896484375).unwrap(),
+                T::from_f64(-0.1294095225509214464043594716713414527475833892822265625).unwrap(),
+            ],
+            lo_r: vec![
+                T::from_f64(0.482962913144690253464119678028509952127933502197265625).unwrap(),
+                T::from_f64(0.836516303737468991386094785411842167377471923828125).unwrap(),
+                T::from_f64(0.2241438680418573470287668669698177836835384368896484375).unwrap(),
+                T::from_f64(-0.1294095225509214464043594716713414527475833892822265625).unwrap(),
+            ],
+            hi_r: vec![
+                T::from_f64(-0.1294095225509214464043594716713414527475833892822265625).unwrap(),
+                T::from_f64(-0.2241438680418573470287668669698177836835384368896484375).unwrap(),
+                T::from_f64(0.836516303737468991386094785411842167377471923828125).unwrap(),
+                T::from_f64(-0.482962913144690253464119678028509952127933502197265625).unwrap(),
+            ],
+        }
+    }
+ }
 
 
-    let n_levels = 4;
+pub struct WaveDecPlanner<T> {
+    signal_length:usize,
+    levels:Vec<usize>,
+    xforms:Vec<WaveletXForm1D<T>>,
+    wavelet: Wavelet<T>,
+    decomp_buffer:Vec<Complex<T>>,
+    signal_buffer:Vec<Complex<T>>,
+}
 
-    let  mut xforms = vec![];
-    let mut sig_len = x.len();
-    for _ in 0..n_levels {
-        let w = WaveletXForm1D::new(sig_len, lo_d.len());
-        sig_len = w.coeff_len;
-        xforms.push(w);
+impl<T> WaveDecPlanner<T>
+where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static {
+    pub fn new(signal_length:usize,n_levels:usize,wavelet:Wavelet<T>) -> Self {
+
+        let mut levels = vec![0;n_levels+2];
+        levels[n_levels+1] = signal_length;
+        let mut xforms = vec![];
+        let mut sig_len = signal_length;
+        for level in 0..n_levels {
+            let w = WaveletXForm1D::<T>::new(sig_len, wavelet.lo_d.len());
+            levels[n_levels - level] = w.coeff_len;
+            sig_len = w.coeff_len;
+            xforms.push(w);
+        }
+        *levels.first_mut().unwrap() = xforms.last().unwrap().coeff_len;
+    
+        let mut decomp_len = xforms.iter().fold(0,|acc,x| acc + x.coeff_len);
+        decomp_len += xforms.last().unwrap().coeff_len;
+    
+        let decomp = vec![Complex::<T>::zero();decomp_len];
+        let signal = decomp.clone();
+
+        Self {
+            signal_length,
+            levels: levels,
+            xforms,
+            wavelet,
+            decomp_buffer:decomp,
+            signal_buffer:signal,
+        }
     }
 
-    let mut decomp_len = xforms.iter().fold(0,|acc,x| acc + x.coeff_len);
-    decomp_len += xforms.last().unwrap().coeff_len;
+    pub fn process(&mut self,signal:&[Complex<T>]) -> Vec<Complex<T>> {
+        let mut stop = self.decomp_buffer.len();
+
+        let lo_d = &self.wavelet.lo_d;
+        let hi_d = &self.wavelet.hi_d;
+
+        self.decomp_buffer[0..self.signal_length].copy_from_slice(&signal[0..self.signal_length]);
+        let mut rl = 0;
+        let mut ru = self.signal_length;
+        for xform in self.xforms.iter_mut() {
+            let start = stop - xform.decomp_len;
+            self.signal_buffer[rl..ru].copy_from_slice(&self.decomp_buffer[rl..ru]);
+            xform.decompose(&self.signal_buffer[rl..ru], lo_d, hi_d, &mut self.decomp_buffer[start..stop]);
+            rl = start;
+            ru = start + xform.coeff_len;
+            stop -= xform.coeff_len;
+        }
+
+        //println!("decomp = {:#?}",self.decomp_buffer);
+
+        self.decomp_buffer.clone()
+
+    }   
+}
 
 
-    println!("decomp_len = {}",decomp_len);
+pub struct WaveRecPlanner<T> {
+    levels:Vec<usize>,
+    xforms:Vec<(usize,usize,WaveletXForm1D<T>)>,
+    signal_length:usize,
+    signal_buffer:Vec<Complex<T>>,
+    approx_buffer:Vec<Complex<T>>,
+    wavelet:Wavelet<T>,
+}
 
-    let mut signal = x.to_owned();
-    let mut stop = decomp_len;
-    let mut decomp = vec![Complex32::zero();decomp_len];
+impl<T> WaveRecPlanner<T>
+where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static {
+    pub fn new(dec_planner:&WaveDecPlanner<T>) -> Self {
 
-    for xform in xforms.iter_mut() {
-        //let mut d = xform.decomp_buffer();
-        let start = stop - xform.decomp_len;
-        xform.decompose(&signal, &lo_d, &hi_d, &mut decomp[start..stop]);
-        // copy the approximation coeffs
-        signal = decomp[start..(start + xform.coeff_len)].to_owned();
-        stop -= xform.coeff_len;
+        let levels = dec_planner.levels.to_owned();
+        let signal_length = dec_planner.signal_length;
+
+        
+        let signal = vec![Complex::<T>::zero();signal_length];
+        let approx = vec![Complex::<T>::zero();signal_length];
+        let xforms:Vec<_> = levels[1..].windows(2).map(|x| {
+            (
+                x[0], // number of approx coeffs
+                x[1], // signal length to reconstruct
+                WaveletXForm1D::<T>::new(x[1],4) // wavelet transform handler
+            )
+        }).collect();
+
+        Self {
+            levels:dec_planner.levels.to_owned(),
+            xforms,
+            signal_length,
+            signal_buffer: signal,
+            approx_buffer: approx,
+            wavelet:dec_planner.wavelet.clone(),
+        }
     }
 
-    println!("decomp len = {}",decomp.len());
-    println!("decomp: {:#?}",decomp);
+    pub fn process(&mut self,decomposed:&[Complex<T>]) -> Vec<Complex<T>> {
+        self.approx_buffer[0..self.levels[0]].copy_from_slice(&decomposed[0..self.levels[0]]);
+        let lo_r = &self.wavelet.lo_r;
+        let hi_r = &self.wavelet.hi_r;
+        let mut start = self.levels[1];
+        for(n_coeffs,sig_len,w) in self.xforms.iter_mut() {
+            let detail = &decomposed[start..(start + *n_coeffs)];
+            start += *n_coeffs;
+            w.reconstruct(&self.approx_buffer[0..*n_coeffs], detail, lo_r, hi_r, &mut self.signal_buffer[0..*sig_len]);
+            self.approx_buffer[0..*sig_len].copy_from_slice(&self.signal_buffer[0..*sig_len]);        }
+        self.signal_buffer.clone()
+    }
 
 }
 
@@ -135,25 +269,32 @@ where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static
         vec![Complex::<T>::zero();self.decomp_len]
     }
 
-    pub fn recon_buffer(&self) -> Vec<Complex<T>>{
-        vec![Complex::<T>::zero();self.sig_len]
+    pub fn recon_buffer(&self,sig_len:usize) -> Vec<Complex<T>>{
+        vec![Complex::<T>::zero();sig_len]
     }
 
     pub fn decompose(&mut self,signal:&[Complex<T>],lo_d:&[T],hi_d:&[T],decomp:&mut [Complex<T>]) {
         symm_ext(signal, self.decomp_ext_len, &mut self.decomp_scratch1);
-        conv1d(&self.decomp_scratch1[0..self.decomp_ext_result_len], lo_d,&mut self.decomp_scratch3,&mut self.decomp_scratch4, &mut self.decomp_scratch2);
+        //conv1d(&self.decomp_scratch1[0..self.decomp_ext_result_len], lo_d,&mut self.decomp_scratch3,&mut self.decomp_scratch4, &mut self.decomp_scratch2);
+        conv_direct(&self.decomp_scratch1[0..self.decomp_ext_result_len], lo_d, &mut self.decomp_scratch2);
         downsample2(&self.decomp_scratch2[conv_valid(self.decomp_ext_result_len, self.filt_len)], &mut decomp[0..self.coeff_len]);
-        conv1d(&self.decomp_scratch1[0..self.decomp_ext_result_len], hi_d,&mut self.decomp_scratch3,&mut self.decomp_scratch4, &mut self.decomp_scratch2);
+        //conv1d(&self.decomp_scratch1[0..self.decomp_ext_result_len], hi_d,&mut self.decomp_scratch3,&mut self.decomp_scratch4, &mut self.decomp_scratch2);
+        conv_direct(&self.decomp_scratch1[0..self.decomp_ext_result_len], hi_d, &mut self.decomp_scratch2);
         downsample2(&self.decomp_scratch2[conv_valid(self.decomp_ext_result_len, self.filt_len)], &mut decomp[self.coeff_len..]);
     }
 
-    pub fn reconstruct(&mut self,decomp:&[Complex<T>],lo_r:&[T],hi_r:&[T],signal:&mut [Complex<T>]) {
-        upsample_odd(&decomp[0..self.coeff_len],&mut self.recon_upsample_scratch);
-        conv1d(&self.recon_upsample_scratch, lo_r, &mut self.recon_scratch3, &mut self.recon_scratch4, &mut self.recon_scratch1);
-        upsample_odd(&decomp[self.coeff_len..],&mut self.recon_upsample_scratch);
-        conv1d(&self.recon_upsample_scratch, &hi_r, &mut self.recon_scratch3, &mut self.recon_scratch4, &mut self.recon_scratch2);
-        let a = &self.recon_scratch1[self.recon_conv_center_lidx..self.recon_conv_center_uidx];
-        let d = &self.recon_scratch2[self.recon_conv_center_lidx..self.recon_conv_center_uidx];
+    pub fn reconstruct(&mut self,approx:&[Complex<T>],detail:&[Complex<T>],lo_r:&[T],hi_r:&[T],signal:&mut [Complex<T>]) {
+
+        let conv_center = _conv_center(self.recon_conv_len, signal.len());
+
+        upsample_odd(approx,&mut self.recon_upsample_scratch);
+        //conv1d(&self.recon_upsample_scratch, lo_r, &mut self.recon_scratch3, &mut self.recon_scratch4, &mut self.recon_scratch1);
+        conv_direct(&self.recon_upsample_scratch, lo_r , &mut self.recon_scratch1);
+        upsample_odd(detail,&mut self.recon_upsample_scratch);
+        //conv1d(&self.recon_upsample_scratch, &hi_r, &mut self.recon_scratch3, &mut self.recon_scratch4, &mut self.recon_scratch2);
+        conv_direct(&self.recon_upsample_scratch, &hi_r, &mut self.recon_scratch2);
+        let a = &self.recon_scratch1[conv_center.0..conv_center.1];
+        let d = &self.recon_scratch2[conv_center.0..conv_center.1];
         signal.iter_mut().enumerate().for_each(|(idx,x)|{
             *x = a[idx] + d[idx];
         });
@@ -251,6 +392,26 @@ where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static {
 }
 
 
+fn conv_direct<T>(input: &[Complex<T>], kernel: &[T], result: &mut [Complex<T>])
+where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static {
+    
+    result.iter_mut().for_each(|x| *x = Complex::<T>::zero());
+
+    let input_len = input.len();
+    let kernel_len = kernel.len();
+    let result_len = input_len + kernel_len - 1;
+
+    for i in 0..result_len {
+        for j in 0..kernel_len {
+            if i >= j && i - j < input_len {
+                result[i] = result[i] + input[i - j] * kernel[j];
+            }
+        }
+    }
+}
+
+
+
 fn downsample2<T>(sig: &[Complex<T>], downsampled: &mut [Complex<T>])
 where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static {
     downsampled.iter_mut().enumerate().for_each(|(idx,r)|{
@@ -287,6 +448,31 @@ where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static
         oup[a - 1 - i] = temp1;
         oup[len2 + a + i] = temp2;
     }
+}
+
+fn per_ext<T>(sig: &[Complex<T>], a: usize, oup: &mut [Complex<T>])
+where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static{
+    let len = sig.len();
+    let mut len2 = len;
+    let mut temp1 = Complex::<T>::zero();
+    let mut temp2 = Complex::<T>::zero();
+
+    for i in 0..len {
+        oup[a + i] = sig[i];
+    }
+
+    if len % 2 != 0 {
+        len2 = len + 1;
+        oup[a + len] = sig[len - 1];
+    }
+
+    for i in 0..a {
+        temp1 = oup[a + i];
+        temp2 = oup[a + len2 - 1 - i];
+        oup[a - 1 - i] = temp2;
+        oup[len2 + a + i] = temp1;
+    }
+
 }
 
 fn conv_valid(sig_len:usize,filt_len:usize) -> Range<usize> {
@@ -355,4 +541,110 @@ where T: FromPrimitive + Copy + Signed + Sync + Send + Debug + 'static
 
 pub fn conv_len(sig_len:usize,filt_len:usize) -> usize {
     sig_len + filt_len - 1
+}
+
+#[test]
+fn test2(){
+    
+    let n = 20;
+
+    let x:Vec<Complex64> = (1..(n+1)).into_iter().map(|x| Complex64::new(x as f64,0.)).collect();
+
+    let lo_d = [
+        -0.1294095225509214464043594716713414527475833892822265625,
+        0.2241438680418573470287668669698177836835384368896484375,
+        0.836516303737468991386094785411842167377471923828125,
+        0.482962913144690253464119678028509952127933502197265625
+    ];
+
+    let hi_d = [
+        -0.482962913144690253464119678028509952127933502197265625000000,
+        0.836516303737468991386094785411842167377471923828125000000000,
+        -0.224143868041857347028766866969817783683538436889648437500000,
+        -0.129409522550921446404359471671341452747583389282226562500000,
+    ];
+
+    let lo_r = [
+        0.482962913144690253464119678028509952127933502197265625000000,
+        0.836516303737468991386094785411842167377471923828125000000000,
+        0.224143868041857347028766866969817783683538436889648437500000,
+        -0.129409522550921446404359471671341452747583389282226562500000,
+    ];
+
+    let hi_r = [
+        -0.129409522550921446404359471671341452747583389282226562500000,
+        -0.224143868041857347028766866969817783683538436889648437500000,
+        0.836516303737468991386094785411842167377471923828125000000000,
+        -0.482962913144690253464119678028509952127933502197265625000000
+    ];
+
+
+    println!("len x = {}",x.len());
+
+    
+    let j = 3;
+    let mut levels = vec![0;j+2];
+    levels[j+1] = x.len();
+    let mut xforms = vec![];
+    let mut sig_len = x.len();
+    for level in 0..j {
+        let w = WaveletXForm1D::<f64>::new(sig_len, lo_d.len());
+        levels[j - level] = w.coeff_len;
+        sig_len = w.coeff_len;
+        xforms.push(w);
+    }
+    *levels.first_mut().unwrap() = xforms.last().unwrap().coeff_len;
+
+
+    let mut decomp_len = xforms.iter().fold(0,|acc,x| acc + x.coeff_len);
+    decomp_len += xforms.last().unwrap().coeff_len;
+
+    println!("decomp_len = {}",decomp_len);
+    //let mut signal = x.to_owned();
+    let mut stop = decomp_len;
+    let mut decomp = vec![Complex64::zero();decomp_len];
+
+    let mut signal = decomp.clone();
+    decomp[0..x.len()].copy_from_slice(&x);
+    let mut rl = 0;
+    let mut ru = x.len();
+    for xform in xforms.iter_mut() {
+        let start = stop - xform.decomp_len;
+        signal[rl..ru].copy_from_slice(&decomp[rl..ru]);
+        xform.decompose(&signal[rl..ru], &lo_d, &hi_d, &mut decomp[start..stop]);
+        rl = start;
+        ru = start + xform.coeff_len;
+        stop -= xform.coeff_len;
+    }
+
+    println!("decomp = {:#?}",decomp);
+
+
+    // initialization
+    // inputs are decomp and levels
+    let mut start = levels[1];
+    let mut signal = vec![Complex64::zero();x.len()];
+    let mut approx = vec![Complex64::zero();x.len()];
+    let mut xforms:Vec<_> = levels[1..].windows(2).map(|x| {
+        (
+            x[0], // number of approx coeffs
+            x[1], // signal length to reconstruct
+            WaveletXForm1D::new(x[1],4) // wavelet transform handler
+        )
+    }).collect();
+
+
+    // the following can be repeated with different decomp arrays without new allocations
+    approx[0..levels[0]].copy_from_slice(&decomp[0..levels[0]]);
+
+    for(n_coeffs,sig_len,w) in xforms.iter_mut() {
+        let detail = &decomp[start..(start + *n_coeffs)];
+        start += *n_coeffs;
+        w.reconstruct(&approx[0..*n_coeffs], detail, &lo_r, &hi_r, &mut signal[0..*sig_len]);
+        approx[0..*sig_len].copy_from_slice(&signal[0..*sig_len]);
+        //approx = signal[0..sig_len].to_owned();
+        println!("signal = {:#?}",signal);
+    }
+
+
 }
